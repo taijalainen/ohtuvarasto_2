6ea8b03085bdf41a8e43ec5156ed2d9e3f932b02
taijalainen <taijalainen@gmail.com>
2024-11-11 16:08:42 +0200

    tehtävä 7

--- osa2/project-reader/src/index.py
+++ osa2/project-reader/src/index.py
@@ -1,10 +1,12 @@
 from project_reader import ProjectReader
+from project import Project
 
 
 def main():
     url = "https://raw.githubusercontent.com/ohjelmistotuotanto-hy/tehtavat/main/viikko2/test-project/pyproject.toml"
     reader = ProjectReader(url)
-    print(reader.get_project())
+    project = reader.get_project()
+    print(project)
 
 
 if __name__ == "__main__":
